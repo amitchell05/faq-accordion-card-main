e043612e54c40702aab145d9465d449728bcda1e
Angela Mitchell <anglarenmit@gmail.com>
2020-10-18 00:56:22 -0500

    Add rotation feature for arrows

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ window.addEventListener("load", function() {
     contentFive.style.display = 'none';
 });
 
-function toggleContent(currentHeading, currentContent) {
+function toggleContent(currentHeading, currentContent, currentArrow) {
     if (currentContent.style.display === 'none') {
         currentHeading.style.fontWeight = 'bold';
         currentContent.style.display = 'block';
+        currentArrow.style.transform = 'rotate(180deg)';
     } else {
         currentHeading.style.fontWeight = 'normal';
         currentContent.style.display = 'none';
+        currentArrow.style.transform = 'rotate(0deg)';
     }
 };
